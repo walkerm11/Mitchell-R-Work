8c253399437dacc73bd623822a9745b19338a5b4
walkerm11 <walkerm11@email.chop.edu>
2017-05-24 09:35:11 -0400

    8th Commit
    Haa!! The changes look a little funny, but they've been recorded
    never-the-less. This is great!

--- First_R_Script_Record.R
+++ First_R_Script_Record.R
@@ -30,11 +30,12 @@ library(irr)
 data(anxiety)
 icc(anxiety, model="twoway", type="agreement")
 
-r1 <- round(rnorm(20, 10, 4))
-r2 <- round(r1 + 10 + rnorm(20, 0, 2))
-r3 <- round(r1 + 20 + rnorm(20, 0, 2))
-icc(cbind(r1, r2, r3), "twoway")              # High consistency
 icc(cbind(r1, r2, r3), "twoway", "agreement") # Low agreement
+icc(cbind(r1, r2, r3), "twoway")              # High consistency
+r3 <- round(r1 + 20 + rnorm(20, 0, 2))
+r2 <- round(r1 + 10 + rnorm(20, 0, 2))
+r1 <- round(rnorm(20, 10, 4))
+
 
 
 # This (below) is going to be very important for this project. 
@@ -43,6 +44,9 @@ test2 <- data_frame(ICC_Data = icc(cbind(r1, r2, r3), "twoway"))
 # I don't know what is going on with this?
 
 # Okay, now I think I understand what's going on. First, make changes in R...
+
+# Now, if I do work in R, and save it...then make the commit...I'll have a record
+# of these changes. For example...see lines 33 > 37. 
 
 
 
